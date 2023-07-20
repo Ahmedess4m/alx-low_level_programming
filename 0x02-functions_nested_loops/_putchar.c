27685f9d99d53219c4a5bf26e97dae5627f7e6b9
Ahmed <ahmedessam12673@gmail.com>
2023-07-19 17:14:28 -0700

    damn

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -5,4 +5,9 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1  returned, and errno is set.
+ * On error, -1 returned, and errno is set.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
